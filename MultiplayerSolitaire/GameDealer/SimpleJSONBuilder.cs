792fdc7080ed0b37369b19251235107daf2564e0
Anton Roy <antonmakesgames@gmail.com>
2021-11-07 16:57:43 +0100

    Added a small json builder

---
+++ MultiplayerSolitaire/GameDealer/SimpleJSONBuilder.cs
@@ -0,0 +1,96 @@
+﻿namespace GameDealer
+{
+    public class SimpleJSONBuilder
+    {
+        private System.Text.StringBuilder stringBuilder;
+        private System.Collections.Generic.List<int> fieldCounter;
+        
+        public SimpleJSONBuilder()
+        {
+            this.stringBuilder = new System.Text.StringBuilder();
+            this.fieldCounter = new System.Collections.Generic.List<int>() { 0 };
+        }
+
+        public SimpleJSONBuilder Start()
+        {
+            this.stringBuilder.Append('{');
+            return this;
+        }
+
+        public SimpleJSONBuilder End()
+        {
+            this.stringBuilder.Append('}');
+            return this;
+        }
+
+        public void Clear()
+        {
+            this.stringBuilder.Clear();
+            this.fieldCounter.Clear();
+            this.fieldCounter.Add(0);
+        }
+
+        public override string ToString()
+        {
+            return this.stringBuilder.ToString();
+        }
+
+        public SimpleJSONBuilder Add(string name, string value)
+        {
+            this.startField();
+            this.appendString(name);
+            this.stringBuilder.Append(':');
+            this.appendString(value);
+            return this;
+        }
+
+        public SimpleJSONBuilder Add(string name, int value)
+        {
+            this.startField();
+            this.appendString(name);
+            this.stringBuilder.Append(':');
+            this.stringBuilder.Append(value);
+            return this;
+        }
+
+        public SimpleJSONBuilder Add(string name, bool value)
+        {
+            this.startField();
+            this.appendString(name);
+            this.stringBuilder.Append(':');
+            this.stringBuilder.Append(value ? "True" : "False");
+            return this;
+        }
+
+        public SimpleJSONBuilder StartObject(string name)
+        {
+            this.startField();
+            this.appendString(name);
+            this.stringBuilder.Append(":{");
+            this.fieldCounter.Add(0);
+            return this;
+        }
+
+        public SimpleJSONBuilder EndObject()
+        {
+            this.stringBuilder.Append('}');
+            this.fieldCounter.RemoveAt(fieldCounter.Count - 1);
+            return this;
+        }
+
+        private void startField()
+        {
+            if (this.fieldCounter[this.fieldCounter.Count - 1] > 0)
+            {
+                this.stringBuilder.Append(',');
+            }
+
+            this.fieldCounter[this.fieldCounter.Count - 1] += 1;
+        }
+
+        private void appendString(string value)
+        {
+            this.stringBuilder.Append('\"').Append(value).Append('\"');
+        }
+    }
+}
