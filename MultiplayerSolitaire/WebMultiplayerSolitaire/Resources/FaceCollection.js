f7f344a1e99b261ad2e6aa29d76ead8cef638e82
Anton Roy <antonmakesgames@gmail.com>
2021-11-24 22:20:16 +0100

    Added new faces

--- MultiplayerSolitaire/WebMultiplayerSolitaire/Resources/FaceCollection.js
+++ MultiplayerSolitaire/WebMultiplayerSolitaire/Resources/FaceCollection.js
@@ -1,7 +1,10 @@
 ﻿class FaceCollection {
     constructor() {
-        this.RootNode = createElementWithClass("div", "FaceCollection");
+        this.RootNode = createElementWithClass("div", "FaceContainer");
+        this.RootNode.appendChild(document.createTextNode("Choose an avatar"));
 
+        this.FaceHolder = createElementWithClass("div", "FaceCollection");
+        this.RootNode.appendChild(this.FaceHolder)
         const xmlhttp = new XMLHttpRequest();
         let instance = this;
         xmlhttp.onload = function () {
@@ -21,7 +24,7 @@
         for (var index = 0; index < this.FacesData.length; ++index) {
             var f = new Face();
             f.Setup(this.FacesData,index , 0);
-            this.RootNode.appendChild(f.RootNode);
+            this.FaceHolder.appendChild(f.RootNode);
             this.Faces[index] = f;
 
 
